fdfe309d527ce9984c48fb0861c1d757430f3420
Pitt Wu <kgb00128@gmail.com>
2022-04-21 23:48:10 +0800

    feat: add timer bg

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="wrapper"></div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0 auto;
+}
+
+.wrapper {
+  min-height: 100vh;
+  background-image: url("@/assets/timer-app-girl-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
 }
 </style>
